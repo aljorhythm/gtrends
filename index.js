e1beffb690d7383ceca00aa273260aa061b49a6b
Joel Lim <103879u@gmail.com>
2018-05-15 15:59:46 +0800

    major update

--- index.js
+++ index.js
@@ -4,7 +4,40 @@ const googleTrends = require("google-trends-api")
 const date = require("date-and-time")
 const _ = require("lodash")
 const fs = require('fs')
-const ArgumentParser = require('argparse').ArgumentParser;
+const ArgumentParser = require('argparse').ArgumentParser
+const Table = require('cli-table')
+const HttpsProxyAgent = require('https-proxy-agent')
+
+// let ip_port = "187.32.184.5:20183"
+// let proxy = new HttpsProxyAgent('https://' + ip_port + '/')
+
+/**
+ * Single call to API
+ * @param {*} options 
+ */
+async function _getKeywordsAndAverages(options) {
+  try {
+    let results = await googleTrends.interestOverTime(options)
+    results = JSON.parse(results)
+    let averages = results.default.averages
+
+    while (averages.length < options.keywords.length) {
+      averages.push(0)
+    }
+    let { keywords } = options
+    averages = _.zip(options.keywords, averages)
+    averages = averages.reduce((obj, keywordAndAverage) => {
+      let keyword = keywordAndAverage[0]
+      let average = keywordAndAverage[1]
+      obj[keyword] = average
+      return obj
+    }, {})
+    
+    return averages
+  } catch (err) {
+    return Promise.reject(err)
+  }
+}
 
 /**
  * Google API url does not accept more than 5 words,
@@ -12,67 +45,158 @@ const ArgumentParser = require('argparse').ArgumentParser;
  * @param {*} object 
  */
 async function getKeywordsAndAverages(options){
-  let keywords = options.keyword
-  let keywordsGroups = _.chunk(keywords, 5);
-  let allPromises = keywordsGroups.map(keywords => {
-    return new Promise((resolve, reject) => {
-      options.keyword = keywords
-      googleTrends.interestOverTime(options, (err, results) => {
-        if(err){ return reject(err) }
-        results = JSON.parse(results)
-        let averages = results.default.averages
-    
-        let keywordsAndAverages = _.zip(keywords, averages)
-        resolve(keywordsAndAverages)
-      })
-    })
+  let { keywords } = options
+  let keywordsGroups = _.chunk(keywords, 5)
+  if (keywordsGroups.length > 1 && keywordsGroups[keywordsGroups.length - 1].length == 1) {
+    keywordsGroups[keywordsGroups.length - 1].push(keywordsGroups[keywordsGroups.length-2].pop())
+  }
+  let allPromises = keywordsGroups.map(keywords => {  
+    return _getKeywordsAndAverages(getQueryOptions(keywords))
   })
-  return Promise.all(allPromises).then((results, err) => {
-    if(err) return reject(err)
-    resultsRegrouped = _.chain(results)
-    .flatten()
-    .concat()
-    .sortBy([(item) => { return item[1] }]).reverse()
-    .value()
-    
-    return resultsRegrouped
-  })
+  try{
+    let results = await Promise.all(allPromises)
+    results = results.reduce((obj, chunkResult) => {
+      return _.merge(obj, chunkResult)
+    }, {})
+    return results
+  } catch(err) {
+    return Promise.reject(err)
+  }
 }
 
 function getQueryOptions(keywords) {
-
   let now =  new Date()
   let tenMonthsAgo = date.addMonths(now, -10)
   let startTime = tenMonthsAgo
   let endTime = now
   let geo = "SG"
 
-  return { keyword: keywords, keywords, startTime, endTime, geo }
+  let options = { keyword: keywords, keywords, startTime, endTime, geo }
+  if(typeof proxy != "undefined" && proxy) {
+    options['agent'] = proxy
+  }
+  return options
 }
 
 async function getTrend(keywords){
-  return new Promise(async (resolve, reject) => {
-    let options = getQueryOptions(keywords)
-    options.keywordsAndAverages = await getKeywordsAndAverages(options)
-    resolve(options)
-  })
+  let options = getQueryOptions(keywords)
+  try {
+    let averages = await getKeywordsAndAverages(options)
+
+    let relatedQueries = await getRelatedQueries(options)
+    
+    let relatedTopics = await getRelatedTopics(options)
+    
+    options.relatedTopics = relatedTopics
+    options.averages = averages
+    options.relatedQueries = relatedQueries
+    return options
+  } catch (err) {
+    throw err
+  }
 }
 
-async function getRelatedQueries(keywords){
+async function _getRelatedTopics(options) {
   return new Promise(async (resolve, reject) => {
-    let options = getQueryOptions(keywords)   
-    
-    googleTrends.relatedQueries(options, (err, results) => {
-      if(err) return reject(err)
+    googleTrends.relatedTopics(options, (err, results) => {
+      if(err) { return reject(err) }
       results = JSON.parse(results)
-      options.relatedQueries = results.default.rankedList
-      resolve(options)  
+      results = results.default.rankedList
+      resolve(results)  
     })  
   })
 }
 
-async function displayTrend(trend){
-  let {keywords, keywordsAndAverages, startTime, endTime, geo} = trend
+async function getRelatedTopics(options) {
+  let keywords = options.keywords
+  let relatedTopics = keywords.reduce((obj, keyword) => {
+    let options = getQueryOptions(keyword)
+    obj[keyword] = _getRelatedTopics(options)
+    return obj
+  }, {})
+
+  await Promise.all(_.values(relatedTopics))
+
+  for (key in relatedTopics) {
+    let relatedToQuery = await relatedTopics[key]
+    relatedToQuery = _.chain(relatedToQuery)
+    .map(row => row['rankedKeyword'])
+    .flatten()
+    .map(item => item.topic.title)
+    .value()
+    relatedTopics[key] = relatedToQuery
+  }
+
+  let results = relatedTopics
+  return results
+}
+
+/**
+ * Single Call
+ * @param {*} options 
+ */
+async function _getRelatedQueries(options) {
+  return new Promise(async (resolve, reject) => {    
+    googleTrends.relatedQueries(options, (err, results) => {
+      if(err) { return reject(err) }
+      results = JSON.parse(results)
+      let relatedQueries = results.default.rankedList
+      resolve(relatedQueries)
+    })  
+  })
+}
+
+async function getRelatedQueries(options) {
+  let keywords = options.keywords
+  let relatedQueries = keywords.reduce((obj, keyword) => {
+    let options = getQueryOptions(keyword)
+    obj[keyword] = _getRelatedQueries(options)
+    return obj
+  }, {})
+
+  await Promise.all(_.values(relatedQueries))
+
+  for (key in relatedQueries) {
+    let relatedToQuery = await relatedQueries[key]
+    relatedToQuery = _.chain(relatedToQuery)
+    .map(row => row['rankedKeyword'])
+    .flatten()
+    .map(item => item.query)
+    .value()
+    relatedQueries[key] = relatedToQuery
+  }
+
+  let results = relatedQueries
+  return results
+}
+
+function displayTrendTable(trend) {
+  let { keywords, averages, relatedQueries, relatedTopics } = trend
+  let table = new Table({
+      head: ["s/n", "keyword", "average", "related queries", "related topics"]
+  })
+  
+  _.chain(keywords).map((keyword, index) => {
+    let row = [keyword, 
+      averages[keyword], 
+      relatedQueries[keyword].slice(0, 10).join("\n"), 
+      relatedTopics[keyword].slice(0, 10).join("\n")]
+    return row
+  })
+  .sortBy(item => item[1])
+  .map((row, index) => {
+    return [index + 1].concat(row)
+  })
+  .value()
+  .reverse()
+  .forEach(row => table.push(row))
+
+  console.log(table.toString())
+}
+
+function displayTrend(trend){
+  let {keywords, startTime, endTime, geo} = trend
+
   console.log("Keywords: ", keywords.join(", "), "\n")
   _.chunk(keywords, 5).forEach((keywords, index) => {
     let url = "https://trends.google.com/trends/explore?"
@@ -85,7 +209,8 @@ async function displayTrend(trend){
   })
   
   console.log("\n")
-  console.table(["keyword", "average"], keywordsAndAverages)
+
+  displayTrendTable(trend)
   console.log("*****************************************************", "\n")
 }
 
@@ -119,8 +244,8 @@ function manipulateQueryLines(queryLines) {
   combo = one.map(function(a1){
     return two.map(function(a2){
       return a1 + a2
-    });
-  });
+    })
+  })
 
   console.log(_.flatten(combo).join(", "))
 }
@@ -130,7 +255,7 @@ async function main(){
     version: '0.0.1',
     addHelp:true,
     description: 'GTRENDS'
-  });
+  })
   parser.addArgument(
     [ '-f', '--file' ],
     {
@@ -141,67 +266,32 @@ async function main(){
   let { file } = parser.parseArgs()
   let filename = file
   let queryLines = await getQueriesLines(filename)
-  let trends = queryLines.map(getTrend)
-  trends = await Promise.all(trends)
-  trends.forEach(displayTrend)
+  let trends
+
+  try {
+    trends = await Promise.all(queryLines.map(getTrend))
+    trends.forEach((trend) => {
+      displayTrend(trend)
+    })
+  } catch (err) {
+    return console.log("Cannot get trends", err)
+  }
 
   // All queries
   console.log("All Queries", "\n")
-  aggregateTrends = trends.map(trend => {
-    let {keywords, keywordsAndAverages} = trend
-    return keywordsAndAverages
-  })
-  aggregateTrends = _.chain(aggregateTrends)
-  .flatten()
-  .sortBy(item => item[1])
-  .value()
-  .reverse()
-  aggregateTrends = aggregateTrends.map(async (trend) => {
-    let keyword = trend[0]
-    result = await getRelatedQueries(trend[0])
-    relatedQueries = _.flatMap(result.relatedQueries, item => item.rankedKeyword)
-    .map(query => query.query)
-    .slice(0, 10)
-    .join("\n")
-    trend.push(relatedQueries)
-    return trend
-  })
-  aggregateTrends = await Promise.all(aggregateTrends)
-
-  var Table = require('cli-table') 
-  var table = new Table({
-      head: ["Query", "Average", "Related Queries"]
-  })
-  aggregateTrends.forEach(row => table.push(row))
-  
-  console.log(table.toString())
-  return console.log(aggregateTrends)
-  
-  console.table(["Term", "Average"], aggregateTrends)
-
-  console.log("*****************************************************", "\n")
-
-  // Related queries
-  console.log("Related Queries", '\n')
-  
-  relatedQueries = aggregateTrends.map(trend => {
-    return getRelatedQueries(trend[0])
-  })
-  relatedQueries = await Promise.all(relatedQueries)
-
-  relatedQueries = relatedQueries.map(async result => {
-    result = await getRelatedQueries(trend[0])
-    queries = _.flatMap(result.relatedQueries, item => item.rankedKeyword)
-    .map(query => query.query)
-    .slice(0, 10)
-    .join("\n")
-    return [result.keywords, queries]
-  })
-
-  relatedQueries = _.sortBy(relatedQueries, item => item[1].length).reverse()
-
-  
-
+  aggregateTrends = trends.reduce((obj, trend) => {
+    let {keywords, averages, relatedTopics, relatedQueries} = trend
+    obj['keywords'] = obj['keywords'] || []
+    obj['keywords'] = obj['keywords'].concat(keywords)
+    obj['averages'] = obj['averages'] || {}
+    obj['averages'] = _.merge(obj['averages'], averages)
+    obj['relatedTopics'] = obj['relatedTopics'] || {}
+    obj['relatedTopics'] = _.merge(obj['relatedTopics'], relatedTopics)
+    obj['relatedQueries'] = obj['relatedQueries'] || {}
+    obj['relatedQueries'] = _.merge(obj['relatedQueries'], relatedQueries)
+    return obj
+  }, {})
+  displayTrendTable(aggregateTrends)
   // manipulateQueryLines(queryLines)
 }
 
